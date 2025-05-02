ed33dfa03a19e225f262b5f39db95b314d20a3bc
smcasau <ciozydelabacau@gmail.com>
2025-05-02 23:19:00 +0300

    script1.js

--- script1.js
+++ script1.js
@@ -3,7 +3,7 @@ const projects = [
         title: "Familia Popescu",
         subtitle: "O familie cu 4 copii",
         mainImage: "https://www.sperantapentruromania.ro/wp-content/uploads/2020/01/poza-buna.jpg",
-        description: "Ajută această familie să treacă prin iarna friguroasă cu un trai",
+        description: "Ajută această familie să treacă prin iarna friguroasă cu un ",
         members: 6,
         children: 4,
         gallery: [
